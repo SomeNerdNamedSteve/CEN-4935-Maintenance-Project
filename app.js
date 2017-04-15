221bc7b2212caf8db2e4311a40bc30225311667b
Steven Burgess <bsteven48@gmail.com>
2017-04-15 03:22:49 -0400

    added Angular.js and verified routing

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 //dependencies
-const express = require('express');
-const path = require('path')
+var express = require('express');
+var path = require('path');
+var engines = require('consolidate')
 
-//constants
-const RUN_PORT = 8080;
-const VIDEO_IN_PORT = 45679;
-const CONTROL_OUT = 45678;
-const app = express();
-const route = express.Router()
+//variables
+var RUN_PORT = 8080;
+var VIDEO_IN_PORT = 45679;
+var CONTROL_OUT = 45678;
+var app = express();
 
-//express html rendering
-app.use(express.static(__dirname+"/public/"));
-app.set('port', (process.env.PORT || RUN_PORT));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //index.html routing
 app.get('/', function(req, res){
@@ -28,7 +26,7 @@ app.get('/error', function(req, res){
 	res.render("error.html");
 });
 
+//showing that the program is running on the RUN_PORT
 app.listen(RUN_PORT, function(){
-    console.log(__dirname);
 	console.log("Running on port " + RUN_PORT);
 });
